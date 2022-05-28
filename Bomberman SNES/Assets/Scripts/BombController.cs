465c02675f3a8434f6f50facdbaecfbf5b7d247a
jamedinaga <jamedinaga@unal.edu.co>
2022-05-28 08:37:22 -0500

    Bomb Controller

---
+++ Bomberman SNES/Assets/Scripts/BombController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombController : MonoBehaviour
+{
+    // This line calls the prefab we have created of the bomb
+    public GameObject bombPrefab;
+    public 
+}
